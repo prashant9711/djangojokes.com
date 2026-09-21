b372d7e6dd1a27dd83e76efe6376d1cdc9cca549
Prashant Pant <prashantpant@isu.edu>
2026-09-21 14:52:45 -0600

    added a new form to submit, update abd display jokes

--- jokes/urls.py
+++ jokes/urls.py
@@ -1,4 +1,12 @@
 from django.urls import path
+from .views import (
+    JokeDetailView, JokeListView, JokeUpdateView, JokeCreateView
+)   
 
 app_name = 'jokes'
-urlpatters = []
+urlpatterns = [
+    path('', JokeListView.as_view(), name='list'),
+    path('joke/<int:pk>/', JokeDetailView.as_view(), name='detail'),
+    path('joke/<int:pk>/update/', JokeUpdateView.as_view(), name='update'),
+    path('joke/create/', JokeCreateView.as_view(), name='create'),
+]
--- jokes/views.py
+++ jokes/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
+
+from .models import Joke
+
+class JokeDetailView(DetailView):
+    model = Joke
+
+class JokeListView(ListView):
+    model = Joke
+
+class JokeCreateView(CreateView):
+    model = Joke
+    fields = ['question', 'answer']
+
+class JokeUpdateView(UpdateView):
+    model = Joke
+    fields = ['question', 'answer']
+
+
 
 # Create your views here.
