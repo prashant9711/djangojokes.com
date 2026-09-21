8007281a2c19270697a51067518008b33296dbf9
Prashant Pant <prashantpant@isu.edu>
2026-09-21 13:06:54 -0600

    Added new apps pages and jokes

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local apps
+    'jokes.apps.JokesConfig',
+    'pages.apps.PagesConfig',
+    
 ]
 
 MIDDLEWARE = [
