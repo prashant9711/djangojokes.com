61bc37f993164b9e6642155e9974ee092beb6d04
Prashant Pant <prashantpant@isu.edu>
2026-09-23 16:18:35 -0600

    Added more stuff to the job application page

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,6 +1,36 @@
 from django import forms
 
 class JobApplicationForm(forms.Form):
+    EMPLOYMENT_TYPES = (
+        (None, ''),
+        ('ft', 'Full-time'),
+        ('pt', 'Part-time'),
+        ('contract', 'Contract work')
+    )
+
+    DAYS = (
+        (1, 'MON'),
+        (2, 'TUE'),
+        (3, 'WED'),
+        (4, 'THU'),
+        (5, 'FRI')
+    )
+
     first_name = forms.CharField()
     last_name = forms.CharField()
     email = forms.EmailField()
+    website = forms.URLField(required=False)
+    employment_type = forms.ChoiceField(choices=EMPLOYMENT_TYPES)
+    start_date = forms.DateField(
+        help_text='The earliest date you can start working.'
+    )
+    available_days = forms.MultipleChoiceField(
+        choices=DAYS,
+        help_text='Select all days that you can work.'
+    )
+
+    desired_hourly_wage = forms.DecimalField()
+    cover_letter = forms.CharField()
+    confirmation = forms.BooleanField(
+        label='I certify that the information that I have provided is true.'
+    )
